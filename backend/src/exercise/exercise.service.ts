6737edff82f3fdd46ff25f843d8c62a982677bfb
J <j@gmail.com>
2024-03-04 18:23:25 +0100

    feat(BE): added exercise/create

--- backend/src/exercise/exercise.service.ts
+++ backend/src/exercise/exercise.service.ts
@@ -1,14 +1,26 @@
 import { db } from "../utils/db.server";
 
 type Exercise = {
-    id: number
-    name: string
-}
+  id: number;
+  isDeadLift: boolean;
+};
 
 export async function listExercises(): Promise<Exercise[]> {
-    //return [{id: 1, name: "xx"}];
-    const exercises: any[] = await db.exercise.findMany({
-        select: { id: true }
-    });
-    return exercises;
+  //return [{id: 1, name: "xx"}];
+  const exercises: any[] = await db.exercise.findMany({
+    select: { id: true },
+  });
+  return exercises;
+}
+
+export async function createExercise(
+  exercise: Omit<Exercise, "id">
+): Promise<Exercise> {
+  const result = await db.exercise.create({
+    data:{
+      isDeadLift:exercise.isDeadLift
+    }
+  });
+
+  return result;
 }
