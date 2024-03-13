9860421e7230c601fe73f79a4934a5ea69203122
Petr Strolený <petr.stroleny@gmail.com>
2024-03-13 23:34:30 +0100

    fix: creating new repetition with repetition entry if none is active

--- backend/src/repetitionEntry/repetition-entry.service.ts
+++ backend/src/repetitionEntry/repetition-entry.service.ts
@@ -3,29 +3,66 @@ import { db } from "../utils/db.server";
 import { RepetitionEntryType } from "./repetition-entry.router";
 
 export async function createRepetitionEntry(value: number, timestamp: number, type?: RepetitionEntryType): Promise<void> {
-    const activeRepetition = await db.repetition.findFirstOrThrow({
+    let activeRepetitionID = 0
+
+    const activeRepetition = await db.repetition.findFirst({
         where: {
             endedAt: null,
         },
         select: {
             id: true,
         }
-    })
+    });
+
+    if (activeRepetition) {
+        activeRepetitionID = activeRepetition.id;
+    } else {
+        const activeSeries = await db.series.findFirstOrThrow({
+            where: {
+                endedAt: null,
+            }
+        });
+
+        const newRepetition = await db.repetition.create({
+            data: {
+                seriesId: activeSeries.id,
+            }
+        });
+        activeRepetitionID = newRepetition.id;
+    }
+
+    const currentTime = new Date(timestamp);
 
     const repetitionCreateData: Prisma.RepetitionEntryCreateInput = Prisma.validator<Prisma.RepetitionEntryCreateInput>()({
         value: value,
-        timestamp: new Date(timestamp),
-        peakRepetition: {connect: {id: activeRepetition.id}}
+        timestamp: currentTime,
+        peakRepetition: {connect: {id: activeRepetitionID}}
     });
 
     if (type) {
         switch(type) {
             case RepetitionEntryType.PEAK: {
-                repetitionCreateData.peakRepetition = {connect: {id: activeRepetition.id}};
+                repetitionCreateData.peakRepetition = {connect: {id: activeRepetitionID}};
+                await db.repetition.update({
+                    where: {
+                        id: activeRepetitionID,
+                    },
+                    data: {
+                        peakedReachedAt: currentTime,
+                    }
+                });
                 break;
             }
             case RepetitionEntryType.END: {
-                repetitionCreateData.endRepetition = {connect: {id: activeRepetition.id}};
+                repetitionCreateData.endRepetition = {connect: {id: activeRepetitionID}};
+                await db.repetition.update({
+                    where: {
+                        id: activeRepetitionID,
+                    },
+                    data: {
+                        endedAt: currentTime,
+                    }
+                });
                 break;
             }
             default: repetitionCreateData.repetition = {connect: {id: activeRepetition.id}};
